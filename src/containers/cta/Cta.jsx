2038ae4bc8dba042abb9cb1ab4d8d84924369acf
psthely <contact@philippesthely.com>
2023-03-01 19:02:38 +0100

    implement Cta container style

--- src/containers/cta/Cta.jsx
+++ src/containers/cta/Cta.jsx
@@ -1,9 +1,27 @@
 import './cta.css'
+import illustration from '../../assets/Illustration.png'
 
 function Cta() {
-    return (
-        <div>Cta</div>
-    )
+  return (
+    <div className='cta'>
+      <div className='cta__content'>
+        <div className='cta__content__head'>
+          <h2>Boost your productivity.<br/>Start using our design kit.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Sed faucibus nunc elementum lectus nulla lacinia turpis.
+            Blandit ligula ac auctor ultricies. Ullamcorper ut enim non in cras bibendum euismod integer.
+          </p>
+        </div>
+        <div className='cta__content__buttons'>
+          <a className='button-primary' href='#'>Get started now</a>
+          <a className='button-secondary' href='#'>Learn more</a>
+        </div>
+      </div>
+      <div className='cta__illustration'>
+        <img src={illustration} alt='cta illustration' />
+      </div>
+    </div>
+  )
 }
 
 export default Cta
