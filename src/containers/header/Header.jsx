a18f27d13ff6af5a2ac2b2d9046a58d561635086
psthely <contact@philippesthely.com>
2023-03-01 14:21:10 +0100

    add header menu container

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,6 +3,22 @@ import './header.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logoCreat from '../../assets/logoCreat.png'
 
+const Tabs = () => (
+  <ul>
+    <li><a href='#'>Home</a></li>
+    <li><a href='#'>About us</a></li>
+    <li><a href='#'>Blog</a></li>
+    <li><a href='#'>Contact</a></li>
+  </ul>
+)
+
+const Cta = () => (
+  <>
+    <a className='button-secondary' href='#'>Log in</a>
+    <a className='button-primary' href='#'>Sign up</a>
+  </>
+)
+
 function Header() {
   const [toggleMenu, setToggleMenu] = useState(false)
 
@@ -12,22 +28,26 @@ function Header() {
         <img src={logoCreat} alt='logo creat' />
       </div>
       <div className='header__tabs'>
-        <ul>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#'>About us</a></li>
-          <li><a href='#'>Blog</a></li>
-          <li><a href='#'>Contact</a></li>
-        </ul>
+        <Tabs />
       </div>
       <div className='header__cta'>
-        <a className='button-secondary' href='#'>Log in</a>
-        <a className='button-primary' href='#'>Sign up</a>
+        <Cta />
       </div>
       <div className='header__menu'>
       { toggleMenu
         ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false) } />
         : <RiMenu3Line color="#000" size={27} onClick={() => setToggleMenu(true) } />
       }
+      {toggleMenu && (
+        <div className='header__menu__content'>
+          <div className='header__menu__tabs'>
+            <Tabs />
+          </div>
+          <div className='header__menu__cta'>
+            <Cta />
+          </div>
+        </div>
+      )}
       </div>
     </div>
   )
