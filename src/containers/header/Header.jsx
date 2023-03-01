23f4171a00f8ab63b71c0c2f121320f0726e1b20
psthely <contact@philippesthely.com>
2023-03-01 11:48:17 +0100

    implement header style

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import './header.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logoCreat from '../../assets/logoCreat.png'
 
 function Header() {
-    return (
-        <div className='header'>
-            <div className='header__logo'>
-                Logo
-            </div>
-            <div className='header__tabs'>
-                Tabs
-            </div>
-            <div className='header__cta'>
-                Cta
-            </div>
-        </div>
-    )
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  return (
+    <div className='header'>
+      <div className='header__logo'>
+        <img src={logoCreat} alt='logo creat' />
+      </div>
+      <div className='header__tabs'>
+        <ul>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>About us</a></li>
+          <li><a href='#'>Blog</a></li>
+          <li><a href='#'>Contact</a></li>
+        </ul>
+      </div>
+      <div className='header__cta'>
+        <a class='button-secondary' href='#'>Log in</a>
+        <a class='button-primary' href='#'>Sign up</a>
+      </div>
+      <div className='header__menu'>
+      { toggleMenu
+        ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false) } />
+        : <RiMenu3Line color="#000" size={27} onClick={() => setToggleMenu(true) } />
+      }
+      </div>
+    </div>
+  )
 }
 
 export default Header
