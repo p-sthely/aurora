d861dfb53cb33b6366fb52c5ca1f97fb42ce539f
psthely <contact@philippesthely.com>
2023-03-02 09:34:03 +0100

    implement the logo clouds section

--- src/containers/logoClouds/LogoClouds.jsx
+++ src/containers/logoClouds/LogoClouds.jsx
@@ -1,9 +1,20 @@
 import './logoClouds.css'
+import { bootstrapLogo, figmaLogo, adobeLogo, nodeLogo, mondayLogo, gitkrakenLogo } from './imports'
 
 function LogoClouds() {
-    return (
-        <div>LogoClouds</div>
-    )
+  return (
+    <div className='logoClouds'>
+      <h2>They trust us !</h2>
+      <div className='logoClouds__list'>
+        <img src={bootstrapLogo} alt='bootstrap logo' />
+        <img src={figmaLogo} alt='figma logo' />
+        <img src={adobeLogo} alt='adobe logo' />
+        <img src={nodeLogo} alt='node logo' />
+        <img src={mondayLogo} alt='monday logo' />
+        <img src={gitkrakenLogo} alt='gitkraken logo' />
+      </div>
+    </div>
+  )
 }
 
 export default LogoClouds
---
+++ src/containers/logoClouds/imports.js
@@ -0,0 +1,15 @@
+import bootstrapLogo from '../../assets/Bootstrap.png'
+import figmaLogo from '../../assets/Figma.png'
+import adobeLogo from '../../assets/Adobe.png'
+import nodeLogo from '../../assets/Node.png'
+import mondayLogo from '../../assets/Monday.png'
+import gitkrakenLogo from '../../assets/Gitkraken.png'
+
+export {
+    bootstrapLogo,
+    figmaLogo,
+    adobeLogo,
+    nodeLogo,
+    mondayLogo,
+    gitkrakenLogo
+}
