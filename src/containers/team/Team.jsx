9590d515905248b665736e4c4fcd282d4c0e37e9
psthely <contact@philippesthely.com>
2023-03-02 10:10:30 +0100

    add team members section

--- src/containers/team/Team.jsx
+++ src/containers/team/Team.jsx
@@ -1,9 +1,52 @@
 import './team.css'
+import { avatar01, avatar02, avatar03, avatar04, avatar05 } from './imports'
 
 function Team() {
-    return (
-        <div>Team</div>
-    )
+  return (
+    <div className='team'>
+      <div className='team__description'>
+        <h2>Our team</h2>
+        <p>Lorem ipsum dolor sit amet consectetur. Scelerisque donec est vitae ut nibh.<br/>Aliquam dui ac dictum et mollis sagittis ut tortor nibh.</p>
+      </div>
+      <div className='team__members'>
+        <div className='team__members__presentation'>
+          <img src={avatar01} alt='ceo avatar' />
+          <div className='team__members__description'>
+            <p className='secondary-text'>CEO</p>
+            <p className='main-text'>Michael Thompson</p>
+          </div>
+        </div>
+        <div className='team__members__presentation'>
+          <img src={avatar02} alt='hr avatar' />
+          <div className='team__members__description'>
+            <p className='secondary-text'>HR</p>
+            <p className='main-text'>Stacey Baker</p>
+          </div>
+        </div>
+        <div className='team__members__presentation'>
+          <img src={avatar03} alt='Head of Design avatar' />
+          <div className='team__members__description'>
+            <p className='secondary-text'>Head of Design</p>
+            <p className='main-text'>Rachael Harper</p>
+          </div>
+        </div>
+        <div className='team__members__presentation'>
+          <img src={avatar04} alt='Head of Marketing avatar' />
+          <div className='team__members__description'>
+            <p className='secondary-text'>Head of Marketing</p>
+            <p className='main-text'>Jessica Noble</p>
+          </div>
+        </div>
+        <div className='team__members__presentation'>
+          <img src={avatar05} alt='Head of Development avatar' />
+          <div className='team__members__description'>
+            <p className='secondary-text'>Head of Development</p>
+            <p className='main-text'>Patrick Carpenter</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Team
---
+++ src/containers/team/imports.js
@@ -0,0 +1,13 @@
+import avatar01 from '../../assets/avatar01.png'
+import avatar02 from '../../assets/avatar02.png'
+import avatar03 from '../../assets/avatar03.png'
+import avatar04 from '../../assets/avatar04.png'
+import avatar05 from '../../assets/avatar05.png'
+
+export {
+    avatar01,
+    avatar02,
+    avatar03,
+    avatar04,
+    avatar05
+}
